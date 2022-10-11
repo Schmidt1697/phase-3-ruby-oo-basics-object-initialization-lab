01fa979d4e7e6e3e8094283c62302452efb8d8d9
Anna Schmidt <marshall1697@gmail.com>
2022-10-11 15:49:03 -0600

    commit tree 7b8ed4510102ff54dad10dea0fc7e7a8c7057355

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Dog
+
+    attr_accessor :name, :breed
+
+    def initialize(name, breed="Mutt")
+        @name = name 
+        @breed = breed  
+    end
+    
+end
+
+
+# fido = Dog.new("Fido", "Aussie")
+# puts fido.breed
